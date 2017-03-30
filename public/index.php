61b5a369ac908f8701d362f692a6d1b5cd4f82e1
JulienMa <mastromonaco.julien@gmail.com>
2017-03-30 14:39:54 +0200

    Add small footer

--- public/index.php
+++ public/index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <?php require "../inc/head.php"; ?>
     </head>
 <body>
-    <header>
-        <?php require "../inc/header.php"; ?>
-    </header>
 
-    <main>
+    <div class="container-fluid">
+        <header>
+            <?php require "../inc/header.php"; ?>
+        </header>
 
-    </main>
+        <main>
+        </main>
 
-    <footer>
-        <?php require "../inc/footer.php"; ?>
-    </footer>
-        <?php require "../inc/scripts.php"; ?>
+        <footer>
+            <?php require "../inc/footer.php"; ?>
+        </footer>
+    </div>
+
+    <?php require "../inc/scripts.php"; ?>
 </body>
 </html>
